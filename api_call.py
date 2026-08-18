a45f1a76dd757e27f3c90886375becd14cc8d798
BMarlinVCU <bjmarlin@vcu.edu>
2026-08-18 19:02:33 +0000

    Just wanted to make it a function

--- api_call.py
+++ api_call.py
@@ -1,31 +1,28 @@
 import requests
 import pandas as pd
 
-# Richmond, Virginia coordinates
-latitude = 37.54
-longitude = -77.44
 
-url = "https://api.open-meteo.com/v1/forecast"
+def get_weather():
 
-params = {
-    "latitude": latitude,
-    "longitude": longitude,
-    "hourly": [
-        "temperature_2m",
-        "relative_humidity_2m",
-        "precipitation",
-        "wind_speed_10m"
-    ],
-    "temperature_unit": "fahrenheit",
-    "wind_speed_unit": "mph",
-    "timezone": "America/New_York"
-}
+    url = "https://api.open-meteo.com/v1/forecast"
 
-response = requests.get(url, params=params)
-response.raise_for_status()
+    params = {
+        "latitude": 37.54,
+        "longitude": -77.44,
+        "hourly": [
+            "temperature_2m",
+            "relative_humidity_2m",
+            "precipitation",
+            "wind_speed_10m"
+        ],
+        "temperature_unit": "fahrenheit",
+        "wind_speed_unit": "mph",
+        "timezone": "America/New_York"
+    }
 
-data = response.json()
+    response = requests.get(url, params=params)
+    response.raise_for_status()
 
-weather = pd.DataFrame(data["hourly"])
+    data = response.json()
 
-print(weather.head(20))
+    return pd.DataFrame(data["hourly"])
